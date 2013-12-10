9844309fd634c4da2d6cc69d56c10f75b6c192a1
Angie The Fairytale <konzeptmeister@gmail.com>
2013-12-10 21:51:19 +0200

    code comments

--- js/TouchArea.js
+++ js/TouchArea.js
@@ -2,67 +2,74 @@ function TouchArea(touchable) {
     this.touchable = touchable;
 
     this.explained = false;
-
     this.who = null;
-    this.newDiv = null;
-    this.newObj = null;
+    //this.newDiv = null; // not used?
+    //this.newObj = null; // not used?
 
     this.timestamp=0;
     this.timeResolution = 100; //ms
 
     // attach listeners
-    //Hammer(this.touchable).on('touch', this.iWannaTouch.bind(this));
     Hammer(this.touchable).on('release', this.iWannaRelease.bind(this));
     Hammer(this.touchable).on('pinch', this.iWannaPinch.bind(this));
     Hammer(this.touchable).on('drag', this.iWannaSwipe.bind(this));
 };
 
-//TouchArea.prototype.iWannaTouch = function () {}
 
 TouchArea.prototype.iWannaPinch = function () {
+    // this function is called everytime on pinch
+    // first, identify the interacting user from Kinect. if no users, then quit
     if (users[users.length - 1] == null) {
         explanation.setText('You do not exist! Dont touch the table! >__<');
         return;
     } else {
+        // else - set the last user
         if (this.who == null) this.who = users[users.length - 1];
     }
 
+    // if the user is not currently creating the object then start creating!
     if (this.who.carries == null && this.who.settingBaseOf == null && this.who.settingHeightOf == null) {
         explanation.setText(this.explained ? null : 'Yeees, like this you make a box base')
 
-
+        // generate random color out of 3 colors with equal probabilities
         switch (Math.round(Math.random() * 3)) {
         case 1: var c = 'green';break;
         case 2: var c = 'blue'; break;
         default: c = 'red'; // for cases 0 and 3
         }
 
-        //console.log(event.gesture.center);
-
+        // get the top left corner of the touch event. this will tell where to create the box in 2D
         var posX = Math.min(event.gesture.touches[0].pageX, event.gesture.touches[1].pageX);
         var posY = Math.min(event.gesture.touches[0].pageY, event.gesture.touches[1].pageY);
 
+        // get the average of two hands from kinect. this will tell where to create the box in 3D
         var coords = {
         x: ((this.who.skeleton.LeftHand.three.position.x + this.who.skeleton.RightHand.three.position.x) / 2),
         y: ((this.who.skeleton.LeftHand.three.position.y + this.who.skeleton.RightHand.three.position.y) / 2),
         z: ((this.who.skeleton.LeftHand.three.position.z + this.who.skeleton.RightHand.three.position.z) / 2)
         };
 
+        // create a new instance of HiddenObject with the calculated coordinates
         this.who.settingBaseOf = new HiddenObject(c, null, coords, {x: posX, y: posY })
         objects.push(this.who.settingBaseOf);
     }
 
 
+    // this part updates the size of the box, when you are creating it
+    // it only works once in a 100 ms or so (can be set above), otherwise we have too many events called
     if (this.timestamp < Date.now() - this.timeResolution) {
         this.timestamp = Date.now();
 
+        // so, if you are currently setting the base of the box
         if (this.who != null && this.who.settingBaseOf != null) {
 
+            // we capure coordinates of touch event
             var x0 = event.gesture.touches[0].pageX;
             var y0 = event.gesture.touches[0].pageY;
             var x1 = event.gesture.touches[1].pageX;
             var y1 = event.gesture.touches[1].pageY;
 
+            // move the object in 3D accordingly
             var moveTo = {
             x: ((this.who.skeleton.LeftHand.three.position.x + this.who.skeleton.RightHand.three.position.x) / 2),
             y: ((this.who.skeleton.LeftHand.three.position.y + this.who.skeleton.RightHand.three.position.y) / 2),
@@ -70,8 +77,10 @@ TouchArea.prototype.iWannaPinch = function () {
             };
             this.who.settingBaseOf.updatePosition3d(moveTo);
 
-            //var scale = Math.round(event.gesture.scale * 100);
+            // move the object in 2D accordingly
             this.who.settingBaseOf.updatePosition2d({x: Math.min(x0, x1), y: Math.min(y0, y1)});
+
+            // update the size of the HiddenObject in 2D ans 3D
             this.who.settingBaseOf.grow(Math.abs(x0-x1), null, Math.abs(y0-y1));
         }
     }
@@ -79,21 +88,26 @@ TouchArea.prototype.iWannaPinch = function () {
 }
 
 TouchArea.prototype.iWannaRelease = function () {
+    // this function is called when you end touch event
+    // switching from setting base of the object to setting its height
+
     if (this.who!=null && this.who.settingBaseOf!=null) {
         explanation.setText(this.explained?null:'Move one hand up to set the height. Then swipe to cut off the box');
-
         this.who.settingHeightOf = this.who.settingBaseOf;
         this.who.settingBaseOf = null;
     }
 }
 
+
 TouchArea.prototype.iWannaSwipe = function () {
+    // this function is called during the swipe touch event (closer to start)
+    // if setting the height of the object now, then save the height
+
     if (this.who!=null && this.who.settingHeightOf!=null){
     explanation.setText(this.explained?null:"Box created. Create more!");
     this.explained = true;
 
     this.who.settingHeightOf.bake();
-
     this.who.settingHeightOf = null;
     this.who = null;
     }
